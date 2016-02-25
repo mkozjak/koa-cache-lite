475c14f2f0cddde7ea5345786241645ea068f8e8
Mario Kozjak <kozjakm1@gmail.com>
2016-02-25 16:51:21 +0100

    support multiple options for particular route
    Signed-off-by: Mario Kozjak <kozjakm1@gmail.com>

--- index.js
+++ index.js
@@ -4,27 +4,75 @@ var Store = require('./lib/store')
 var responseKeys = [ 'status', 'message', 'header', 'body' ]
 
 module.exports = (routes, opts) => {
-  if (opts.debug) console.info('cache options:', routes, opts)
+  // if (opts.debug) console.info('cache options:', routes, opts)
 
   opts.expireOpts = new Map()
   opts.callCnt = new Map()
   opts.defaultTimeout = 5000
   var store = new Store(opts)
 
+  for (let key of Object.keys(routes)) {
+    // validate and reorganize route values
+    if (typeof routes[key] !== 'object'
+      && typeof routes[key] !== 'boolean'
+      && routes[key] !== 'increasing'
+      && isNaN(routes[key])) {
+
+      if (opts.debug) console.info('invalid value for key', key)
+
+      delete routes[key]
+      continue
+    }
+
+    if (!isNaN(routes[key])) {
+      routes[key] = { timeout: routes[key] }
+    }
+
+    if (routes[key].cacheKeyArgs instanceof Array) {
+      if (opts.debug) console.info('cacheKeyArgs of array type not supported:', key)
+      delete routes[key]
+      continue
+    }
+
+    if (typeof routes[key].cacheKeyArgs === 'string') {
+      routes[key].cacheKeyArgs = {
+        custom: routes[key].cacheKeyArgs
+      }
+    }
+
+    if (routes[key].cacheKeyArgs && typeof routes[key].cacheKeyArgs.headers === 'string') {
+      routes[key].cacheKeyArgs.headers = [ routes[key].cacheKeyArgs.headers ]
+    }
+
+    if (routes[key].cacheKeyArgs && typeof routes[key].cacheKeyArgs.query === 'string') {
+      routes[key].cacheKeyArgs.query = [ routes[key].cacheKeyArgs.query ]
+    }
+
+    // parse caching route params
+    if (key.indexOf(':') !== -1) {
+      routes[key]['regex'] = new RegExp(
+        key.replace(/:[A-z0-9]+/g, '[A-z0-9]+')
+        .replace(/^\//, '^\/')
+        .replace(/$/, '(?:\/)?$')
+        .replace(/\//g, '\\/'))
+
+    }
+  }
+
   let routeKeys = Object.keys(routes)
   let routeKeysLength = routeKeys.length
 
   // set default increasing options if not defined
   let incrs
+
   if (opts.increasing !== undefined) incrs = opts.increasing
-  else
-    incrs = {
-      1: 1000,
-      3: 2000,
-      10: 3000,
-      20: 4000,
-      50: 5000
-    }
+  else incrs = {
+    1: 1000,
+    3: 2000,
+    10: 3000,
+    20: 4000,
+    50: 5000
+  }
 
   let cntStep = Object.keys(incrs)
 
@@ -40,65 +88,51 @@ module.exports = (routes, opts) => {
       if (!routeKeysLength) return yield next;
 
       // create key
-      let request_url = this.request.url
+      let requestKey = this.request.url
 
-      if (opts.keyFragment !== undefined) {
-        if (this.request.header[opts.keyFragment] !== undefined) {
-          request_url = request_url.concat("@", this.request.header[opts.keyFragment])
-        }
-        else {
-          if (opts.debug) console.warn('Key fragment "%s" not set!', opts.keyFragment)
-          return yield next
-        }
-      }
-
+      // first pass - exact match
       for (let i = 0; i < routeKeysLength; i++) {
         let key = routeKeys[i]
 
-        if (request_url.indexOf(key) != -1) {
-          let routeExpire = routes[key]
+        if (key === this.request.url) {
+          if (opts.debug)
+            console.info('matched route:', this.request.url, routes[routeKeys[i]].regex)
 
-          if (routeExpire == false) {
-            return yield next
-          }
+          if (routes[key].cacheKeyArgs)
+            requestKey = yield setRequestKey(requestKey, key, this.request)
 
-          if (isNaN(routeExpire) && (typeof routeExpire !== 'boolean') && (routeExpire !== 'increasing')) {
-            if (opts.debug) console.warn('invalid cache setting:', routeExpire)
-            return yield next
-          }
+          let ok = yield setExpires(i, requestKey)
+          if (!ok) return yield next
 
-          // override default timeout
-          if (typeof routeExpire === 'boolean') routeExpire = opts.defaultTimeout
-          else if (routeExpire === 'increasing') {
-            let count = opts.callCnt.has(request_url)
+          break
+        }
+      }
 
-            if (count) {
-              count = opts.callCnt.get(request_url) + 1
-              opts.callCnt.set(request_url, count)
-              let steps = cntStep.length
+      // second pass - regex match
+      for (let i = 0; i < routeKeysLength; i++) {
+        let key = routeKeys[i]
 
-              for (let i = 0; i < steps; i++) {
-                if (count == cntStep[i]) {
-                  opts.expireOpts.set(request_url, incrs[cntStep[i]])
-                  break
-                }
-              }
-            }
-            else {
-              opts.callCnt.set(request_url, 1)
-              opts.expireOpts.set(request_url, incrs[cntStep[0]])
-            }
-          }
-          else opts.expireOpts.set(request_url, routeExpire)
+        if (!routes[routeKeys[i]].regex) continue
+
+        if (routes[routeKeys[i]].regex.test(this.request.url)) {
+          if (opts.debug)
+            console.info('matched route:', this.request.url, routes[routeKeys[i]].regex)
+
+          if (routes[key].cacheKeyArgs)
+            requestKey = yield setRequestKey(requestKey, key, this.request)
+
+          let ok = yield setExpires(i, requestKey)
+          if (!ok) return yield next
+
           break
         }
 
-        if (i == routeKeys.length - 1) return yield next
+        if (i === routeKeys.length - 1) return yield next
         else continue
       }
 
       // check if no-cache is provided
-      if (this.request.header['cache-control'] == 'no-cache') {
+      if (this.request.header['cache-control'] === 'no-cache') {
         return yield next
       }
 
@@ -107,24 +141,24 @@ module.exports = (routes, opts) => {
         for (let i = 0; i < routeKeysLength; i++) {
           let key = routeKeys[i]
 
-          if (request_url.indexOf(key) != -1) {
-            store.delete(request_url)
+          if (requestKey.indexOf(key) != -1) {
+            store.delete(requestKey)
           }
         }
         return yield next
       }
 
       // return cached response
-      let exists = yield store.has(request_url)
+      let exists = yield store.has(requestKey)
 
       if (exists) {
-        let item = yield store.get(request_url)
+        let item = yield store.get(requestKey)
 
-        if ('string' == typeof(item)) item = JSON.parse(item)
-        if (opts.debug) console.info('returning from cache for url', request_url)
+        if ('string' === typeof(item)) item = JSON.parse(item)
+        if (opts.debug) console.info('returning from cache for url', requestKey)
 
         for (let key in item) {
-          if (key == 'header') {
+          if (key === 'header') {
             let value = item[key]
 
             for (let hkey in value) {
@@ -150,14 +184,89 @@ module.exports = (routes, opts) => {
           _response[key] = this.response[key]
       }
 
-      if (opts.debug) console.info('caching', request_url)
+      if (opts.debug) console.info('caching', requestKey)
 
       // set new caching entry
-      store.set(request_url, _response)
+      store.set(requestKey, _response)
     }
     catch (error) {
       if (opts.debug) console.error(error)
       this.throw(error)
     }
+  }
+
+  function *setExpires(routeKeysIndex, requestKey) {
+    let routeExpire = routes[routeKeys[routeKeysIndex]].timeout
+
+    if (routeExpire === false) {
+      return false
+    }
+
+    // TODO: move outside to the 'validate' part
+    if (isNaN(routeExpire) && typeof routeExpire !== 'boolean') {
+        if (opts.debug) console.warn('invalid cache setting:', routeExpire)
+        return false
+    }
+
+    // override default timeout
+    if (typeof routeExpire === 'boolean') routeExpire = opts.defaultTimeout
+    else if (routeExpire === 'increasing') {
+      let count = opts.callCnt.has(requestKey)
+
+      if (count) {
+        count = opts.callCnt.get(requestKey) + 1
+        opts.callCnt.set(requestKey, count)
+        let steps = cntStep.length
+
+        for (let i = 0; i < steps; i++) {
+          if (count === cntStep[i]) {
+            opts.expireOpts.set(requestKey, incrs[cntStep[i]])
+            break
+          }
+        }
+      }
+      else {
+        opts.callCnt.set(requestKey, 1)
+        opts.expireOpts.set(requestKey, incrs[cntStep[0]])
+      }
+    }
+    else opts.expireOpts.set(requestKey, routeExpire)
+
+    return true
+  }
+
+  function *setRequestKey(requestKey, routeKey, ctx) {
+    // append specified http headers to requestKey
+    if (routes[routeKey].cacheKeyArgs.headers instanceof Array) {
+      requestKey += '#'
+
+      for (let name of routes[routeKey].cacheKeyArgs.headers) {
+        requestKey += ctx.header[name]
+      }
+    }
+
+    // ...or append all http headers to requestKey
+    else if (routes[routeKey].cacheKeyArgs.headers === true) {
+      requestKey += '#'
+
+      for (let name of Object.keys(ctx.headers)) {
+        requestKey += ctx.headers[name]
+      }
+    }
+
+    // append specified http url query parameters to requestKey
+    if (routes[routeKey].cacheKeyArgs.query instanceof Array) {
+      requestKey += '?'
+
+      for (let name of Object.keys(ctx.query)) {
+        requestKey += ctx.query[name]
+      }
+    }
+
+    // ...or append all http url query parameters to requestKey
+    else if (routes[routeKey].cacheKeyArgs.query === true)
+      requestKey += '?' + ctx.querystring
+
+    return requestKey
   }
 }
